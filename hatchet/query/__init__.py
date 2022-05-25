7a8cbd1c52288e9e7c81f199d83c55e0843896da
ilumsden <lumsden.ian@gmail.com>
2022-05-25 00:19:40 -0400

    Redefines the bitwise operators for Query to use compound queries

--- hatchet/query/__init__.py
+++ hatchet/query/__init__.py
@@ -4,16 +4,34 @@
 # SPDX-License-Identifier: MIT
 
 from .engine import QueryEngine
-from .function import FunctionQuery
+from .query import Query
 from .string import StringQuery
 from .object import ObjectQuery
 from .compound import (
     NaryQuery,
     AndQuery,
     IntersectionQuery,
+    ConjunctionQuery,
+    conjunction_op,
     OrQuery,
     UnionQuery,
+    DisjunctionQuery,
+    disjunction_op,
     XorQuery,
     SymDifferenceQuery,
+    ExcDisjunctionQuery,
+    exc_disjunction_op,
     NotQuery,
+    ComplementQuery,
+    complement_op,
 )
+
+# Set the following bitwise operators for Query:
+#  * __and__ (q1 & q2)
+#  * __or__ (q1 | q2)
+#  * __xor__ (q1 ^ q2)
+#  * __invert__ (~q)
+setattr(Query, "__and__", conjunction_op)
+setattr(Query, "__or__", disjunction_op)
+setattr(Query, "__xor__", exc_disjunction_op)
+setattr(Query, "__invert__", complement_op)
